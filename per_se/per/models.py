1f63e9eefccd7b444fec1018fd57b8c7974f6e04
git config --global user.email "gjing@calpoly.edu <git config --global user.name "George Jing>
2014-07-21 14:40:21 -0700

    Added some models
    put in a to do list (which is done) and added in all the required
    linking tables

--- per_se/per/models.py
+++ per_se/per/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# models
 class Symbol(models.Model):
     symbol = models.CharField(max_length=50)
     name = models.CharField(max_length=100)
@@ -13,9 +13,67 @@ class Equation(models.Model):
     name = models.CharField(max_length=150)
     def __unicode__(self):
     	return self.name
+    	
+class Section(models.Model):
+	name = models.CharField(max_length=100)
+	ch_num = models.IntegerField()
+	def __unicode__(self):
+		return self.name
 
+class Exercise(models.Model):
+	sec = models.ForeignKey(Section)
+	text = models.TextField()
+	img = models.CharField(max_length = 100)
+	title = models.CharField(max_length = 100)
+	def __unicode__(self):
+		return self.title
+
+class Tag(models.Model):
+	name = models.CharField(max_length=100)
+	def __unicode__(self):
+		return self.name
+		
+#Linking Tabels
+
+#Link symbols and equations
 class SymbolEquation(models.Model):
 	symbol = models.ForeignKey(Symbol)
 	equation = models.ForeignKey(Equation)
 	def __unicode__(self):
 		return self.symbol.symbol + " : " +  self.equation.latex
+		
+#Link Sections and Equations
+class SectionEqn(models.Model):
+	section = models.ForeignKey(Section)
+	equation = models.ForeignKey(Equation)
+	def __unicode__(self):
+		return self.section.name + " : " +  self.equation.latex
+
+#Link sections and symbols
+class SectionSym(models.Model):
+	section = models.ForeignKey(Section)
+	symbol = models.ForeignKey(Symbol)
+	def __unicode__(self):
+		return self.section.name + " : " +  self.symbol.symbol
+
+#Link tags and exercises
+class TagLink(models.Model):
+	tag = models.ForeignKey(Tag)
+	exc = models.ForeignKey(Exercise)
+	def __unicode__(self):
+		return self.exc.title + " : " +  self.tag.name
+
+#Link equations and exercises
+class ExcEqn(models.Model):
+	exc = models.ForeignKey(Exercise)
+	eqn = models.ForeignKey(Equation)
+	def __unicode__(self):
+		return self.exc.title + " : " +  self.eqn.name
+
+#Link exercises and symbols
+class ExcSym(models.Model):
+	exc = models.ForeignKey(Exercise)
+	symbol = models.ForeignKey(Symbol)
+	type = models.IntegerField() #0 is given, 1 is hidden, 2 is find
+	def __unicode__(self):
+		return self.exc.title + " : " +  self.symbol.symbol
