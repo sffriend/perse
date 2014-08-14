ab329449cb21b79c5d4b15051d4a1c3b0fe43cd6
git config --global user.email "gjing@calpoly.edu <git config --global user.name "George Jing>
2014-08-14 15:00:54 -0700

    updated urls

--- per_se/per_se/urls.py
+++ per_se/per_se/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
+from per import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,6 +10,17 @@ urlpatterns = patterns('',
     # url(r'^$', 'per_se.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^per/', include('per.urls')),
+    url(r'^$', views.index, name='index'),
+    url(r'^symbols/$', views.symbols, name='symbols'),
+    url(r'^equations/$', views.equations, name='equations'),
+    url(r'^sections/$', views.sections, name='section'),
+    url(r'^exercises/$', views.exercises, name='exercises'),
+    url(r'^symbols/(?P<per_id>\d+)/$', views.symDetail, name='symDetail'),
+    url(r'^equations/(?P<per_id>\d+)/$', views.eqnDetail, name='eqnDetail'),
+    url(r'^sections/(?P<per_id>\d+)/$', views.secDetail, name='secDetail'),
+    url(r'^exercises/(?P<per_id>\d+)/$', views.excDetail, name='excDetail'),
+    url(r'^exclist/$', views.exclist, name='exclist'),
+    url(r'^secexclist/$', views.SecExc, name='secexclist'),
+    url(r'^exclist/results$', views.listResults, name='excListResults'),
     url(r'^admin/', include(admin.site.urls)),
 )
